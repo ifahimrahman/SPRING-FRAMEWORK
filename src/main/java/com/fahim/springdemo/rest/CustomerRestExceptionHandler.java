b096ba5a8718210b4439c5a0017eadba5a16fae9
Fahim Rahman <ifahim.rahman@gmail.com>
2021-05-18 18:53:15 +0600

    initial commit

---
+++ src/main/java/com/fahim/springdemo/rest/CustomerRestExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.fahim.springdemo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+
+    // Add an exception handler for CustomerNotFoundException
+    @ExceptionHandler
+    public ResponseEntity <CustomerErrorResponse > handleException (CustomerNotFoundException exc){
+
+        // create customerErrorResponse
+        CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.NOT_FOUND.value(),
+                exc.getMessage(),
+                System.currentTimeMillis());
+
+        //Return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // Add another Exception handler . . .  to catch any exception (catch all)
+    @ExceptionHandler
+    public ResponseEntity <CustomerErrorResponse > handleException (Exception exc){
+
+        // create customerErrorResponse
+        CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                exc.getMessage(),
+                System.currentTimeMillis());
+
+        //Return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/fahim/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.fahim.springdemo.service;
+
+import com.fahim.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
