78e734c2c91e2b999ff25067d9c7d22541047721
Fahim Rahman <ifahim.rahman@gmail.com>
2021-05-18 20:39:06 +0600

    initial commit

---
+++ src/main/java/com/fahim/springboot/employeemanager/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.fahim.springboot.employeemanager.service;
+
+
+import com.fahim.springboot.employeemanager.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+    public Employee findById(int theId);
+    public void save(Employee theEmployee);
+    public void deleteById(int theId);
+}
